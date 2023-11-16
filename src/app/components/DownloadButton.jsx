dfe91eac875fbea95de2cf56a1bdd3ed0fefa5c5
ciromos11x <142385426+ciromos11x@users.noreply.github.com>
2023-11-16 12:22:54 +0100

    fix

--- src/app/components/DownloadButton.jsx
+++ src/app/components/DownloadButton.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const DownloadButton = () => {
   const handleDownload = () => {
-    const fileUrl = '/ciro-porto/public/curriculum.pdf'; // Percorso del file nella cartella public
+    const fileUrl = '/ciro-porto/public/cv.PNG'; // Percorso del file nella cartella public
 
     // Creare un link di download
     const downloadLink = document.createElement('a');
